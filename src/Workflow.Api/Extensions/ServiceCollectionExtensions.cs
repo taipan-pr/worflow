bf4e4843d2760ccd9f63a4b8633aeae0b6a2f8d9
Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>
2024-02-13 00:09:23 +0700

    Setup docker compose file

---
+++ src/Workflow.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+using Asp.Versioning;
+using Workflow.Api.Swagger;
+
+namespace Workflow.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1);
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new UrlSegmentApiVersionReader(),
+                new HeaderApiVersionReader("X-Api-Version"));
+        }).AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            // Add a custom operation filter which sets default values
+            options.OperationFilter<SwaggerDefaultValues>();
+        });
+
+        return services;
+    }
+}
